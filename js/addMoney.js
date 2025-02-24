9e415bcffc91c2ef51fd168780eb41cefcbe38e0
mdasifalijihat <mdasifalijihat@gmail.com>
2025-02-24 23:46:26 +0600

    add money section success

--- js/addMoney.js
+++ js/addMoney.js
@@ -6,9 +6,13 @@ document.getElementById('add-money-btn').addEventListener('click', function(even
     const convertedAmount = parseFloat(amount);
     const pin = document.getElementById('pin').value;
     const convertedPin = parseInt(pin);
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const convertedMainBalance = parseFloat(mainBalance);
 
     if(convertedPin === 1234){
-        console.log('Pin okay')
+        const sum = convertedMainBalance + convertedAmount;
+        document.getElementById('main-balance').innerText = sum;
+        alert('Add success!')
     }else{
         alert('Wrong Pin')
     }
